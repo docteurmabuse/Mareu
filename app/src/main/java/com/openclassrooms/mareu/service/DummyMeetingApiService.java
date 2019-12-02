ac40b80872c138e10d76d4d1263ec3d5387da204
docteurmabuse <laurent.tizzone@gmail.com>
2019-12-02 19:01:23 +0100

    - rchange behavior of DummyMeetingClas

--- app/src/main/java/com/openclassrooms/mareu/service/DummyMeetingApiService.java
+++ app/src/main/java/com/openclassrooms/mareu/service/DummyMeetingApiService.java
@@ -3,17 +3,11 @@ package com.openclassrooms.mareu.service;
 import com.openclassrooms.mareu.model.Meeting;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class DummyMeetingApiService implements MeetingApiService {
-    public static List<Meeting> DUMMY_MEETING = Arrays.asList(
-    );
-    private static List<Meeting> meetings = generateNeighbours();
+    public static final List<Meeting> meetings = new ArrayList<Meeting>();
 
-    static List<Meeting> generateNeighbours() {
-        return new ArrayList<>(DUMMY_MEETING);
-    }
 
     @Override
     public List<Meeting> getMeetings() {
