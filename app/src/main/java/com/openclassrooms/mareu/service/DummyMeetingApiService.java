5fc8f0b6b415bf8a37c50b4830bd41781cbba1bc
docteurmabuse <laurent.tizzone@gmail.com>
2019-12-24 11:16:16 +0100

    - correct bug with dummy generator and filter doens not filter enw Date(); -> change behavior of getPlaceFilterMeetings in api service

--- app/src/main/java/com/openclassrooms/mareu/service/DummyMeetingApiService.java
+++ app/src/main/java/com/openclassrooms/mareu/service/DummyMeetingApiService.java
@@ -3,9 +3,11 @@ package com.openclassrooms.mareu.service;
 import com.openclassrooms.mareu.model.Meeting;
 import com.openclassrooms.mareu.ui.meeting_list.util.FiltersContent;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import static com.openclassrooms.mareu.service.FakeMeetingGenerator.generateMeetings;
 
@@ -40,11 +42,11 @@ public class DummyMeetingApiService implements MeetingApiService {
     @Override
     public List<Meeting> getPlaceFilteredMeetings(Date fDate, List<FiltersContent.Places> fPlaces) {
         List<Meeting> fMeetings = new ArrayList<>();
-        /*SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy", Locale.FRANCE);
+        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy", Locale.FRANCE);
         String mDate1 = null;
         if (fDate != null) {
             mDate1 = df.format(fDate);
-        }*/
+        }
         if (fPlaces.size() > 0 && fDate == null) {
             for (Meeting meeting : getMeetings()) {
                 for (FiltersContent.Places places : fPlaces) {
@@ -55,17 +57,17 @@ public class DummyMeetingApiService implements MeetingApiService {
             }
         } else if (fPlaces.size() > 0) {
             for (Meeting meeting : getMeetings()) {
-                //String mDate2 = df.format(meeting.getmDate());
+               String mDate2 = df.format(meeting.getmDate());
                 for (FiltersContent.Places places : fPlaces) {
-                    if (meeting.getmPlace().contains(places.getpName()) && meeting.getmDate().equals(fDate)) {
+                    if (meeting.getmPlace().contains(places.getpName()) &&mDate2.equals(mDate1)) {
                         fMeetings.add(meeting);
                     }
                 }
             }
         } else if (fPlaces.size() < 1) {
             for (Meeting meeting : getMeetings()) {
-                //   String mDate2 = df.format(meeting.getmDate());
-                if (meeting.getmDate().equals(fDate)) {
+                  String mDate2 = df.format(meeting.getmDate());
+                if (mDate2.equals(mDate1)) {
                     fMeetings.add(meeting);
                 }
             }
