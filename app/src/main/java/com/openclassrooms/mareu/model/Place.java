ccc0aae527095f2f325b1cd41851d87451ca660e
docteurmabuse <laurent.tizzone@gmail.com>
2020-01-22 19:02:15 +0100

    - added again Fake_Place in Place for Tests purpose which was deleted by error

--- app/src/main/java/com/openclassrooms/mareu/model/Place.java
+++ app/src/main/java/com/openclassrooms/mareu/model/Place.java
@@ -2,6 +2,9 @@ package com.openclassrooms.mareu.model;
 
 import androidx.annotation.NonNull;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Helper class for providing sample content for user interfaces created by
  * Android template wizards.
@@ -12,6 +15,23 @@ public class Place {
 
     @NonNull
     private final String name;
+
+    /**
+     * An array of Meetings Place List items.
+     */
+    public static final List<Place> FAKE_PLACES = Arrays.asList(
+            new Place(1, "Birdo"),
+            new Place(2, "Daisy"),
+            new Place(3, "Donkey Kong"),
+            new Place(4, "Harmonie"),
+            new Place(5, "Luigi"),
+            new Place(6, "Mario"),
+            new Place(7, "Peach"),
+            new Place(8, "Toad"),
+            new Place(9, "Wario"),
+            new Place(10, "Yoshi")
+    );
+
 
     private Place(Integer id, @NonNull String name) {
         this.id = id;
